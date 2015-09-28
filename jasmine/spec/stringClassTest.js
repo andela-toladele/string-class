b510008fb6375d02a176ca18b061b151b4d8278a
andela-toladele <toyindipol@gmail.com>
2015-09-28 12:41:35 +0100

    Some String class prototypes

---
+++ jasmine/spec/stringClassTest.js
@@ -0,0 +1,77 @@
+describe('String class test', function() {
+
+
+  describe('hasVowels function', function() {
+
+    it('should return false for string not containing vowel', function() {
+
+      expect("h_sV_w_ls".hasVowels()).toBe(false);
+    });
+
+    it('should return false for string not containing vowel', function() {
+
+      expect("hasVowels".hasVowels()).toBe(true);
+    });
+  });
+
+  describe('toUpper function', function() {
+
+    it('should return string all in upper case', function() {
+
+      expect("toUpper".toUpper()).toBe('TOUPPER');
+    });
+  });
+
+  describe('toLower function', function() {
+
+    it('should return string all in upper case', function() {
+
+      expect("tOLoWer".toLower()).toBe('tolower');
+    });
+  });
+
+  describe('isQuestion function', function() {
+
+    it('should return false for string that does not end with ?', function() {
+
+      expect("is it a questi?on".isQuestion()).toBe(false);
+    });
+
+    it('should return true for string that does end with ?', function() {
+
+      expect("is it a question?".isQuestion()).toBe(true);
+    });
+  });
+
+  describe('words function', function() {
+
+    it('should return array of words in a string', function() {
+
+      expect("should return array of words in a string".words()).toEqual([ 'should', 'return', 'array', 'of', 'words', 'in', 'a', 'string' ]);
+    });
+  });
+
+  describe('wordCount function', function() {
+
+    it('should return number of words in a string', function() {
+
+      expect("should return array of words in a string".wordCount()).toEqual(8);
+    });
+  });
+
+  describe('toCurrency function', function() {
+
+    it('should return a string formatted in currency mode', function() {
+
+      expect(3773763.12.toCurrency()).toEqual('3,773,763.12');
+    });
+  });
+
+  describe('toCurrency function', function() {
+
+    it('should return a string formatted in currency mode', function() {
+
+      expect('3,773,763.12'.fromCurrency()).toBe(3773763.12);
+    });
+  });
+});
---
+++ jasmine/src/stringClass.js
@@ -0,0 +1,67 @@
+String.prototype.hasVowels = function() {
+
+  //Tests if string contains any of 'a,e,i,o,u'
+  //Returns true if yes, else returns false
+  return /[aeiou]/.test(this);
+}
+
+//Replaces any upper case letter found in a string
+//with lower case equivalent
+String.prototype.toUpper = function() {
+
+  //Gets the ascii code of matched upper case alphabet, adds 32 from it to
+  //get ascii code of equivalent lower case alphabet and converts it to alphabet
+  return this.replace(/[a-z]/g, function(x) {
+    return String.fromCharCode(x.charCodeAt(0) - 32);
+  });
+
+}
+
+//Replaces any lower case letter found in a string
+//with upper case equivalent
+String.prototype.toLower = function() {
+
+  //Gets the ascii code of matched lower case alphabet, subtracts 32 from it to
+  //get ascii code of equivalent upper case alphabet and converts it to alphabet
+  return this.replace(/[A-Z]/g, function(x) {
+    return String.fromCharCode(x.charCodeAt(0) + 32);
+  });
+}
+
+//Tests if a string ends with '?'
+//Returns true if yes, else return false
+String.prototype.isQuestion = function() {
+
+  return /\?$/.test(this);
+}
+
+//Return array of words in a string
+String.prototype.words = function() {
+
+  //The pattern matches white space which breaks word
+  var pattern = /\s+/g;
+
+  //Splits a string into an array with white space as delimiter
+  return this.split(pattern);
+}
+
+//Returns the length of array of words in a string
+String.prototype.wordCount = function() {
+  return this.words().length;
+}
+
+//Converts a number to currency mode
+Number.prototype.toCurrency = function() {
+
+  //Macthes any digit(and remembers it) that is followed by three digits or (x * 3) digits
+  //where x is a positive integer, then followed by '.'
+  var pattern = /(\d)(?=(\d{3})+(?=\.))/g
+
+  //Replaces the matched digit with same digit followed by ','
+  return this.toString().replace(pattern, "$1,");
+};
+
+//Replaces all "," with ""
+String.prototype.fromCurrency = function() {
+  return Number(this.replace(/,/g, ""));
+}
